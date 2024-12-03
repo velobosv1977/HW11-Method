572e4e9bc967501784a79dca933e30de88787d24
velobosv1977 <velobosv33@gmail.com>
2024-12-03 15:13:45 +0300

    Initial Commit HW-11

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Домашнее задание 11. Методы");
+        System.out.println("");
+        //Task 1.
+        System.out.println("Задача 1. Определение високосного года реализацией метода");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+    }
+}
